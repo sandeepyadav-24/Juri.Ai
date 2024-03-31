7bf43a66d7d58679f2ae4dd7803c5a9dd492d987
sandeepyadav-24 <103882286+sandeepyadav-24@users.noreply.github.com>
2024-03-31 12:18:48 +0530

    Complete Landing Page Structure

--- src/component/HeroSection.tsx
+++ src/component/HeroSection.tsx
@@ -1,18 +1,22 @@
 const HeroSection = () => {
   return (
-    <div>
-      <h1>
+    <div className="mx-60 my-20">
+      <h1 className="text-center text-4xl font-bold">
         Revolutionize Legal Workflows with AI-Driven Contract Generation &
         Document
       </h1>
-      <h1>
+      <h1 className="text-center my-3">
         groundbreaking solution for lawyers, utilizing advanced generative AI to
         automate tedious tasks and simplify legal processes. Say goodbye to
         time-consuming contract drafting and document comprehension – our
         platform empowers lawyers and individuals alike, saving valuable time
         and resources.
       </h1>
-      <button>Start </button>
+      <button className="px-4 py-2 rounded-3xl bg-[#284AF2] text-white ">
+        Sign Up{" "}
+      </button>
     </div>
   );
 };
+
+export default HeroSection;
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,12 +1,19 @@
 const Navbar = () => {
   return (
     <div className="flex flex-row justify-between  mx-10 my-5 ">
-      <div className="font-bold text-2xl">Juri.Ai</div>
+      <div className=" flex flex-row justify-between">
+        <div className="font-bold text-4xl mx-7">Juri.Ai</div>
+        <div className="flex flex-row">
+          <div className="mx-3 my-2">Advocates</div>
+          <div className="mx-3 my-2">Pricing </div>
+          <div className="mx-3 my-2">ChatBot</div>
+        </div>
+      </div>
       <div className="flex flex-row  ">
-        <div className="mx-3">Apple</div>
-        <div className="mx-3">Banana</div>
-        <div className="mx-3">Canoploy</div>
-        <div className="mx-3">Dealer</div>
+        <div className="mx-3 text-[#284AF2] border-[1px] border-[#284AF2] px-3 py-2 rounded-3xl">
+          Sign Up
+        </div>
+        <div className="mx-3 my-2">Login</div>
       </div>
     </div>
   );
